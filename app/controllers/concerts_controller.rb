9d2ec7d93b708bace61d4be4e57f5e1802c8f649
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-18 10:20:14 -0500

    Switched database from SQL to PS

---
+++ app/controllers/concerts_controller.rb
@@ -0,0 +1,28 @@
+class ConcertsController < ApplicationController
+
+	def index
+		@concerts_today = Concert.where("date < ?", Date.tomorrow)
+		# @concerts_latermonth = Concert.where("date > ?", Date.tomorrow).where("date < ?", DateTime.end_of_month)
+	# concerts_all.each do |concert|
+	# 	if concert.date < DateTime.current.end_of_day
+	# 		@concerts_today.push(concert)
+	# 	elsif (concert.date > DateTime.end_of_month) && (concert.date < DateTime.current.end_of_day)
+	# 		@concerts_latermonth << concert
+	# 	end
+	# end
+
+	end
+
+	def new
+		@concert = Concert.new
+	end
+
+	def edit
+		@concert = Concert.find_by(id: params[:id])
+	end
+
+	def show
+		@concert = Concert.find_by(id: params[:id])
+	end
+	
+end
